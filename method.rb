842b3bcc12027cb6028f60011f4c28ae2d12416d
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-05-19 14:40:10 -0500

    testing my_select

--- method.rb
+++ method.rb
@@ -1,38 +1,38 @@
-def my_each (array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
-  return array
-end
+# def my_each (array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i += 1
+#   end
+#   return array
+# end
 
 test_array = (1...7).to_a
 # print test_array
 
-my_each (test_array) do |item|
-  print item
-end
+# my_each (test_array) do |item|
+#   print item
+# end
 
-def my_each_with_index (array)
-  i = 0
-  while i < array.length
-    yield array[i] , i
-    i += 1
-  end
-  return array
-end
+# def my_each_with_index (array)
+#   i = 0
+#   while i < array.length
+#     yield array[i] , i
+#     i += 1
+#   end
+#   return array
+# end
 
-my_each_with_index (test_array) do |item , inx|
-  item = inx * 2
-  puts item
-end
+# my_each_with_index (test_array) do |item , inx|
+#   item = inx * 2
+#   puts item
+# end
 
 def my_select (array)
   returned_array = []
-  condition = yield
   i = 0
   while i < array.length
+    condition = yield i
     if condition
       returned_array << array[i]
     end
@@ -40,4 +40,11 @@ def my_select (array)
   end
   return returned_array
 end
+
+my_select (test_array) do |indx|
+  return true if test_array[indx].even?
+end
+
+puts my_select (test_array)
+
 
