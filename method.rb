f4c358e4de15cec04f93282ebaa73463d5775d66
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-05-19 12:04:10 -0500

    passing a block to test my_each method

--- method.rb
+++ method.rb
@@ -1,8 +1,15 @@
 def my_each (array)
   i = 0
-  while i < array.lenght
+  while i < array.length
     yield array[i]
     i += 1
   end
   return array
+end
+
+test_array = (1..7).to_a
+# print test_array
+
+my_each (test_array) do |item|
+  print item
 end
