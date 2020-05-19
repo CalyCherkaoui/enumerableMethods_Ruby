44973b0bfc69bf61a76843895fdb4c901659dfdd
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-05-19 12:19:51 -0500

    creating my_each_with_index  method

--- method.rb
+++ method.rb
@@ -7,9 +7,23 @@ def my_each (array)
   return array
 end
 
-test_array = (1..7).to_a
+test_array = (1...7).to_a
 # print test_array
 
 my_each (test_array) do |item|
   print item
+end
+
+def my_each_with_index (array)
+  i = 0
+  while i < array.length
+    yield array[i] , i
+    i += 1
+  end
+  return array
+end
+
+my_each_with_index (test_array) do |item , inx|
+  item = inx * 2
+  puts item
 end
